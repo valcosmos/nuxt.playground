92d8eaadb2705eaa10688e7e44c77c093077b266
Cupid Valentine <westerngod@126.com>
2024-01-21 16:13:40 +0800

    chore: basic module setup

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { x, y } = useMouse()
+const color = useColorMode()
+
+function toggleDark() {
+  color.value = color.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <div>
+    hello world {{ x }} {{ y }}
+    <button @click="toggleDark">
+      {{ color.value }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
