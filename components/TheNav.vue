6a4b7539bb5cb21cfa93c30913f39a246fe6e157
Cupid Valentine <westerngod@126.com>
2024-02-05 22:09:40 +0800

    feat: align with nuxt.com center

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -3,11 +3,11 @@
 </script>
 
 <template>
-  <nav class="flex gap-2 items-center px6 py2 text-lg border-b border-base">
+  <nav class="flex gap-2 items-center px6 py3 text-lg border-b border-base">
     <div class="i-logos-nuxt-icon text-4xl" />
     <span text-2xl font-mono>nuxt.playground</span>
     <div flex-auto />
-    <NuxtLink p2 rounded hover="bg-active" href="https://" target="_blank">
+    <NuxtLink p2 rounded hover="bg-active" href="https://github.com/valcosmos/nuxt.playground" target="_blank">
       <div class="i-carbon-logo-github" />
     </NuxtLink>
   </nav>
