bea2da0a70366957085f0dacdf592b7f0badff21
Cupid Valentine <westerngod@126.com>
2024-02-05 21:12:31 +0800

    chore: update deps

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <nav class="flex gap-2 items-center px6 py4 text-lg border-b border-base">
+  <nav class="flex gap-2 items-center px6 py2 text-lg border-b border-base">
     <div class="i-logos-nuxt-icon text-4xl" />
     <span text-2xl font-mono>nuxt.playground</span>
     <div flex-auto />
